7d9762c80740c73b76cb0308835db8cb6c725b7c
Trent Walkiewicz <t.walkiewicz@gmail.com>
2011-03-05 02:14:28 -0800

    added rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SKrEXP::Application.config.secret_token = '08815cdc2c2643e87014cb21075a2871a62bd1b94cdca28cfb4c1067fa17971a906e0ba39ef6741111af7637fe8ddbbbd39c5bbf9a6504593d8ef517e1a262a0'
