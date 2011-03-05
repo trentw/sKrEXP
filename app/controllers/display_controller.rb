d81489af7cac90a06ec6c86f61bde3ef9dad0e6f
Trent Walkiewicz <t.walkiewicz@gmail.com>
2011-03-05 07:39:11 -0800

    modified page layout

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,5 +1,17 @@
 class DisplayController < ApplicationController
+  layout 'display'
+  
+  require 'net/http'
+  require 'uri'
+  
   def recent
+    uri = URI.parse("http://mooncolony.org/json")
+    response = Net::HTTP.get_response(uri)
+    @page_title = "recently played"
+    @response = ""
   end
 
+  def favorites
+    @page_title = "favorites"
+  end
 end
